f3431fcfa6d8057a69293d28254f40e65b8c4bdd
bruno-martins-costa <bruno.martins.costa@outlook.com>
2020-08-14 19:08:17 -0300

    Updates

---
+++ precache-manifest.b2fdb74edb46bf2df158dc6357a8e902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdefad00cae6f76f05263c9bd681c3f8",
+    "url": "/tic-tac-toe-class/index.html"
+  },
+  {
+    "revision": "bf3a0c9a7097095720d2",
+    "url": "/tic-tac-toe-class/static/css/main.11c0e877.chunk.css"
+  },
+  {
+    "revision": "9a51d812d2273e342c11",
+    "url": "/tic-tac-toe-class/static/js/2.f32a7659.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-class/static/js/2.f32a7659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3a0c9a7097095720d2",
+    "url": "/tic-tac-toe-class/static/js/main.3279e083.chunk.js"
+  },
+  {
+    "revision": "526cbbf388e8a75188dd",
+    "url": "/tic-tac-toe-class/static/js/runtime-main.4f0fad40.js"
+  }
+]);
